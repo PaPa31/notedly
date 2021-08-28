5e4b47a41530cb5f81d115f136510f956b839d7b
papa31 <visidoor@yandex.ru>
2021-08-28 20:12:59 +0500

    feat: auth & state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+ import {
+    ApolloClient,
+    ApolloProvider,
+    createHttpLink,
+    InMemoryCache
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
 import GlobalStyle from '/components/GlobalStyle';
 
-
 // Настраиваем API URI и кэш
- const uri = process.env.API_URI;
- const cache = new InMemoryCache();
- // Настраиваем Apollo Client
+const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
+const cache = new InMemoryCache();
+
+// Проверяем наличие токена и возвращаем заголовки в контекст
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: localStorage.getItem('token') || ''
+        }
+    };
+})
+
+ // Создаем клиент Apollo
 const client = new ApolloClient({
-    uri,
+    link: authLink.concat(httpLink),
     cache,
+    resolvers: {},
     connectToDevTools: true
 })
+
+// Проверяем наличие локального токена
+ const data = {
+ isLoggedIn: !!localStorage.getItem('token')
+ };
+ // Записываем кэшированные данные при начальной загрузке
+ cache.writeData({ data });
+ // Записываем данные кэша после его сброса 
+ client.onResetStore(() => cache.writeData({ data }));
 
 const App = () => (
     <ApolloProvider client={client}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Favorites from './favorites';
 import Home from './home';
 import MyNotes from './mynotes';
 import NotePage from './note';
+import SignIn from './signin';
+import SignUp from './signup';
 
+
+const IS_LOGGED_IN = gql`
+{
+    isLoggedIn @client
+}
+`
 
 const Pages = () => {
     return (
@@ -15,9 +24,39 @@ const Pages = () => {
                 <Route path="/mynotes" component={MyNotes} />
                 <Route path="/favorites" component={Favorites} />
                 <Route path="/note/:id" component={NotePage} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/signin" component={SignIn} />
             </Layout>
         </Router>
     )
+}
+
+// Добавляем компонент PrivateRoute под компонентом 'Pages'
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const { loading, error, data } = useQuery(IS_LOGGED_IN);
+    // Если данные загружаются, выводим сообщение о загрузке
+    if (loading) return <p>Loading...</p>;
+    // Если при получении данных произошел сбой, выводим сообщение об ошибке
+    if (error) return <p>Error!</p>;
+    // Если пользователь авторизован, направляем его к запрашиваемому компоненту
+    // В противном случае перенаправляем на страницу авторизации
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                data.isLoggedIn === true ? (
+                    <Component {...props} />
+                ) : (
+                        <Redirect
+                            to={{
+                                pathname: '/signin',
+                                state: { from: props.location }
+                            }}
+                        />
+                    )
+            }
+        />
+    );
 }
 
 export default Pages
