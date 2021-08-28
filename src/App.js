2c6ca94ab39ef7bdc53a4315f687bb703399d3b2
papa31 <visidoor@yandex.ru>
2021-08-28 12:36:19 +0500

    feat: add styled.components

--- src/App.js
+++ src/App.js
@@ -1,2 +1,15 @@
-// index.js
-// This is the main entry point of our application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import GlobalStyle from '/components/GlobalStyle';
+
+const App = () => {
+    return (
+        <div>
+            <GlobalStyle />
+            <Pages />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
